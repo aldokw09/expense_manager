1ecd5a748c9a2ee5f19d293aacfa4fc512d867c9
aldokurnia09 <aldokw09@gmail.com>
2021-03-06 15:31:44 +0700

    First Commit

--- conn/connection.php
+++ conn/connection.php
@@ -1,7 +1,7 @@
 <?php
-$server ="localhost";
-$username = "root";
-$password = "";
-$database = "expense_manager";
+$server ="us-cdbr-east-03.cleardb.com";
+$username = "b579de5c169153";
+$password = "2d18e757";
+$database = "heroku_10c4060c3a1f751";
 $con = mysqli_connect($server, $username, $password, $database) or die ("failed to connect");
 ?>
